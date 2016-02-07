5b7e7c8e82cd36c0af2edcc4276010dc53286bfd
Joshua Manricks <josh@thesecretlocation.com>
2016-02-07 17:28:37 -0500

    damage area

---
+++ Assets/Scripts/Utilities/DamageArea.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DamageArea : MonoBehaviour {
+
+	public List<LifeController> damageTargets;
+
+	public float damage = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach( LifeController l in damageTargets) {
+			l.TakeDamage(damage);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		damageTargets.Add(other.gameObject.GetComponent<LifeController>());
+	}
+
+
+	void OnTriggerExit(Collider other) {
+		damageTargets.Remove(other.gameObject.GetComponent<LifeController>());
+	}
+}
