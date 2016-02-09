b01ae2d6358e7844a923af8fb8ea9ec2c20f05d5
Joshua Manricks <josh@thesecretlocation.com>
2016-02-09 13:14:59 -0500

    gam ui

---
+++ Assets/Scripts/GameUI/LifeUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeUI : MonoBehaviour {
+
+
+	GameObject heart;
+	GameObject skull;
+
+	// Use this for initialization
+	void Start () {
+		heart = transform.GetChild (0).gameObject;
+		skull = transform.GetChild (1).gameObject;
+		skull.SetActive (false);
+	}
+
+	public void Die() {
+		heart.SetActive (false);
+		skull.SetActive (true);
+	}
+
+	public void Reset() {
+		heart.SetActive (true);
+		skull.SetActive (false);
+	}
+}
